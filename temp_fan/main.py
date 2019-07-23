e0e210321638cb50b7742ea33b185157f48dec76
wayne <zowiegong@gmail.com>
2019-07-23 22:35:13 +0800

    update

--- temp_fan/main.py
+++ temp_fan/main.py
@@ -5,10 +5,16 @@ from threading import Timer
 from gpiozero import LED
 from flask import Flask, Response
 
+FAN_GPIO = 4
+SERVER_CHAN_TOKEN = ''
+
+temp_max = 55
+temp_min = 50
+temp_warning = 60
 
 logging.basicConfig(level=logging.INFO, filename='temp_fan.log', filemode='a', format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 app = Flask(__name__)
-fan = LED(4)
+fan = LED(FAN_GPIO)
 
 
 def get_temp ():
@@ -24,16 +30,16 @@ def get_fan_status ():
 
 
 def set_fan (temp, fan_status):
-  if temp > 55 and not(fan_status):
+  if temp > temp_max and not(fan_status):
     logging.info("%s℃ FAN ON." % temp)
     fan.on()
-  elif temp < 50 and fan_status:
+  elif temp < temp_min and fan_status:
     logging.info("%s℃ FAN OFF." % temp)
     fan.off()
   
-  if temp > 60:
+  if temp > temp_warning:
     logging.warning("TEMP: %s℃" % temp)
-    os.system("curl https://sc.ftqq.com/{TOKEN}.send?text=树莓派温度警告:%s度" % temp)
+    os.system("curl https://sc.ftqq.com/%s.send?text=树莓派温度警告:%s度" % (SERVER_CHAN_TOKEN, temp))
 
 
 def interval ():
@@ -44,6 +50,11 @@ def interval ():
 @app.route('/api/tempfan')
 def tempfan():
   result = {
+    'config': {
+    'tempWarning': temp_warning,
+    'tempMax': temp_max,
+    'tempMin': temp_min,
+    },
     'temp': get_temp(),
     'fanStatus': get_fan_status()
   }
